dc8a3c373ccd0f3b60fe39d86e6c1c97811d96e8
umermalik053 <um4273209@gmail.com>
2024-07-16 02:09:31 +0500

    done

--- app.js
+++ app.js
@@ -8,23 +8,17 @@ let questionout = document.querySelector(".qustion-out-of");
 let questionNumber = document.querySelector(".q-number");
 let startDiv = document.querySelector(".start-div");
 let quiz = document.querySelector(".container");
-let restart = document.querySelector(".Restart_Quiz");
 let home = document.querySelector(".Home");
 let questionEl = document.querySelector(".Question");
-
 let option1 = document.querySelector("#option1");
 let option2 = document.querySelector("#option2");
 let option3 = document.querySelector("#option3");
 let option4 = document.querySelector("#option4");
-
-let answers = document.querySelectorAll(".answer")
-
+let answers = document.querySelectorAll(".answer");
 let next = document.querySelector("#next");
 
 let scores = 0;
 let currentQuestion = 0;
-
-
 
 const render = () => {
   let questionindex = quizQuestions[currentQuestion].question;
@@ -36,23 +30,18 @@ const render = () => {
 
     if (i == 0) {
       option1.innerHTML = items;
-      document.getElementById('ans1').value = items;
+      document.getElementById("ans1").value = items;
     } else if (i == 1) {
       option2.innerHTML = items;
-      document.getElementById('ans2').value = items;
+      document.getElementById("ans2").value = items;
     } else if (i == 2) {
       option3.innerHTML = items;
-      document.getElementById('ans3').value = items;
+      document.getElementById("ans3").value = items;
     } else {
       option4.innerHTML = items;
-      document.getElementById('ans4').value = items;
+      document.getElementById("ans4").value = items;
     }
   }
-
-
-  
-
-
 
   questionout.innerHTML = `Question ${currentQuestion + 1} Out of ${
     quizQuestions.length
@@ -79,11 +68,27 @@ const startQuizFunc = () => {
 
   let interval = setInterval(() => {
     if (min === 0 && sec === 0) {
+      let percent = (scores / (quizQuestions.length - 1)) * 100;
+      let percentround = Math.round(percent);
+
+      if (percentround < 70) {
+        Swal.fire({
+          icon: "error",
+          title: "You Fail , Time's up!",
+          text: `Your Percentage is ${percentround}% `,
+        });
+      } else {
+        Swal.fire({
+          title: "Good! You Passed, Time's up!",
+          text: `You Percentage is ${percentround}% `,
+          icon: "success",
+        });
+      }
+
       clearInterval(interval);
-      alert("Time's up! You lose!");
       scoreDiv.style.display = "block";
       quiz.style.display = "none";
-      scoreEl.innerHTML = `Your score is ${scores}`;
+      scoreEl.innerHTML = `Your score is ${scores}<br><br>Percentage is ${percentround}%`;
     } else if (sec === 0) {
       min--;
       sec = 59;
@@ -95,16 +100,14 @@ const startQuizFunc = () => {
 };
 
 const deselectAll = () => {
-  answers.forEach((currAnsElem) => currAnsElem.checked = false)
- 
-}
+  answers.forEach((currAnsElem) => (currAnsElem.checked = false));
+};
 
-next.addEventListener("click", () => {
-
+const nextFun = () => {
   const question = quizQuestions[currentQuestion];
   const correctAnswer = question.correctAnswer;
   let selectedAnswer;
-  
+
   for (const answer of answers) {
     if (answer.checked) {
       selectedAnswer = answer.value;
@@ -116,30 +119,37 @@ next.addEventListener("click", () => {
     scores++;
   }
 
-
   if (currentQuestion < quizQuestions.length - 1) {
     currentQuestion++;
-    deselectAll()
+    deselectAll();
     render();
   } else {
+    let percent = (scores / (quizQuestions.length - 1)) * 100;
+    let percentround = Math.round(percent);
     scoreDiv.style.display = "block";
     quiz.style.display = "none";
-    scoreEl.innerHTML = `Your score is ${scores}`;
+    scoreEl.innerHTML = `Your score is ${scores}<br><br>Percentage is ${percentround}%`;
+    if (percentround < 70) {
+      Swal.fire({
+        icon: "error",
+        title: "You Fail",
+        text: `Your Percentage is ${percentround}% `,
+      });
+    } else {
+      Swal.fire({
+        title: "Good! You Passed",
+        text: `You Percentage is ${percentround}% `,
+        icon: "success",
+      });
+    }
+    currentQuestion = 0;
+    scores = 0;
   }
+};
 
-
-});
-
-
-
+next.addEventListener("click", nextFun);
 
 home.addEventListener("click", () => {
-  scoreDiv.style.display = "none";
-  startDiv.style.display = "block";
-  startDiv.style.margin = " auto";
+  window.location.reload();
 });
-
-
-
-restart.addEventListener("click", startQuizFunc);
 startquiz.addEventListener("click", startQuizFunc);
